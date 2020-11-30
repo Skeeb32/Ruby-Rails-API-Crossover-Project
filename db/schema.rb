5303dfa1780d824528c81a045f83434665a43bc1
Skeeb32 <skeeb32@gmail.com>
2020-11-29 20:49:53 -0800

    Updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_29_064043) do
+ActiveRecord::Schema.define(version: 2020_11_30_043141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "cats", force: :cascade do |t|
+  create_table "events", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "pokemons", force: :cascade do |t|
-    t.string "name"
+    t.string "event_type"
+    t.datetime "at"
+    t.string "button_color"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
